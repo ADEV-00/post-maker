62485758ef9a4158df639dec0dc9382345f6bfd2
unknown <developer.amel@gmail.com>
2021-01-05 12:42:53 +0100

    Added post create screen and installed view show and camera roll

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { PostCreateScreen } from "./src/screens/PostCreate";
+import PostCreateScreen from "./src/screens/PostCreateScreen";
 
 export default function App() {
   return <PostCreateScreen />;
---
+++ src/screens/PostCreateScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const PostCreateScreen = () => {
+  return (
+    <View style={{ marginTop: 200 }}>
+      <Text>Post Create Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default PostCreateScreen;
