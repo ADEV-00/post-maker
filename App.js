4c687a16747ce8eecef5bab3d0bda02219b8f444
unknown <developer.amel@gmail.com>
2021-01-06 21:46:06 +0100

    Working with permissions

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import PostCreateScreen from "./src/screens/PostCreateScreen";
+import TestScreen from "./src/TestScreen";
 
 export default function App() {
-  return <PostCreateScreen />;
+  return <TestScreen />;
 }
