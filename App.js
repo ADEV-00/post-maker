f58f52642ad3f80f1acbf66ea76c4a14ebcc0ba5
unknown <developer.amel@gmail.com>
2021-01-07 22:18:33 +0100

    NEw

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import PostCreateScreen from "./src/screens/PostCreateScreen";
-import TestScreen from "./src/TestScreen";
+import React from 'react';
+import PostCreateScreen from './src/Screens/PostCreateScreen';
 
 export default function App() {
-  return <TestScreen />;
+  return <PostCreateScreen />;
 }
--- src/screens/PostCreateScreen.js
+++ src/screens/PostCreateScreen.js
@@ -1,104 +1,12 @@
-import React, { useRef, useState } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Platform,
-  PermissionsAndroid,
-  TouchableOpacity,
-  Alert,
-} from "react-native";
-import { captureRef } from "react-native-view-shot";
-/* import CamerRoll from "@react-native-community/cameraroll"; */
-import * as MediaLibrary from "expo-media-library";
-import * as Permissions from "expo";
-import Expo, { Constants, Camera } from "expo";
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
 const PostCreateScreen = () => {
-  //Create a ref for the commponent
-  const viewRef = useRef();
-  const [rollGranted, setRollGranted] = useState(false);
-
-  /* const reqPremission = async () => {
-    const { status } = await MediaLibrary.requestPermissionsAsync();
-    if (status === "granted") {
-      console.log("You have a permission");
-    } else {
-      console.log("Something went wrong");
-    }
-  };
-
-  const saveImage = async () => {
-    try {
-      const uri = await captureRef(viewRef, {
-        format: "png",
-        quality: 0.8,
-      });
-      const per = await reqPremission();
-      const image = CamerRoll.save(uri, "photo");
-      if (image) {
-        Alert.alert(
-          "",
-          "Image saved successfully.",
-          [{ text: "OK", onPress: () => {} }],
-          { cancelable: false }
-        );
-      }
-    } catch (err) {
-      console.log("Error", err);
-    }
-  }; */
-
-  const reqPremission = async () => {
-    const { Permissions } = Expo;
-    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    if (status === "granted") {
-      setRollGranted(true);
-    } else {
-      setRollGranted(false);
-      console.log("Uh oh! The user has not granted us permission.");
-    }
-  };
-
-  const saveImage = async () => {
-    await reqPremission();
-    const uri = await captureRef(viewRef, {
-      format: "png",
-      quality: 0.8,
-    });
-    const asset = await MediaLibrary.createAssetAsync(uri);
-    MediaLibrary.createAlbumAsync("Expo", asset)
-      .then(() => {
-        Alert.alert("Album created");
-      })
-      .catch((err) => {
-        Alert.alert("An Error Occurred");
-      });
-  };
-
   return (
-    <View style={{ marginTop: 200 }}>
-      <View
-        style={{
-          width: "100%",
-          height: 100,
-          backgroundColor: "orange",
-          borderRadius: 40,
-          padding: 50,
-        }}
-        ref={viewRef}
-      >
-        <Text>This is going to be saved in gallery of device</Text>
-      </View>
-      <View>
-        <TouchableOpacity onPress={() => reqPremission()}>
-          <Text>Save image</Text>
-        </TouchableOpacity>
-      </View>
+    <View>
+      <Text style={{fontSize: 100}}>THIS ISTE SREEN</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default PostCreateScreen;
