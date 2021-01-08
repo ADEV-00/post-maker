444eeaaf090540857f7174720761d1a124d993e2
unknown <developer.amel@gmail.com>
2021-01-08 12:23:59 +0100

    Added permissions and saveing image to camera roll

--- src/screens/PostCreateScreen.js
+++ src/screens/PostCreateScreen.js
@@ -1,12 +1,99 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useRef} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  PermissionsAndroid,
+  Platform,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
+
+import ViewShot, {captureRef} from 'react-native-view-shot';
+import CameraRoll from '@react-native-community/cameraroll';
 
 const PostCreateScreen = () => {
+  const viewRef = useRef();
+
+  //Get Permission
+  const getPermissionAndroid = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+        {
+          title: 'Image Download Permission',
+          message: 'Your permission is required to save images to your device',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        return true;
+      }
+      Alert.alert(
+        '',
+        'Your permission is required to save images to your device',
+        [{text: 'OK', onPress: () => {}}],
+        {cancelable: false},
+      );
+    } catch (err) {
+      console.log('Error', err);
+    }
+  };
+
+  //Save image to Device
+
+  const downloadImage = async () => {
+    try {
+      // react-native-view-shot caputures component
+      const uri = await captureRef(viewRef, {
+        format: 'png',
+        quality: 0.8,
+      });
+
+      if (Platform.OS === 'android') {
+        const granted = await getPermissionAndroid();
+        if (!granted) {
+          return;
+        }
+      }
+
+      // cameraroll saves image
+      const image = CameraRoll.save(uri, 'photo');
+      if (image) {
+        Alert.alert(
+          '',
+          'Image saved successfully.',
+          [{text: 'OK', onPress: () => {}}],
+          {cancelable: false},
+        );
+      }
+      console.log(image);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
   return (
-    <View>
-      <Text style={{fontSize: 100}}>THIS ISTE SREEN</Text>
+    <View
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+      }}>
+      <ViewShot
+        ref={viewRef}
+        style={{width: '100%', height: 200, backgroundColor: 'red'}}>
+        <Text style={{fontSize: 50, color: 'white'}}>This is snap view</Text>
+        <TouchableOpacity onPress={() => downloadImage()}>
+          <Text>Save Imgage</Text>
+        </TouchableOpacity>
+      </ViewShot>
     </View>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default PostCreateScreen;
