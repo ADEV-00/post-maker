00f4a3198ae389c21b6d1a092f6e9913cc458bc6
unknown <developer.amel@gmail.com>
2021-01-24 23:46:15 +0100

    Added Landing Screen

---
+++ src/screens/LandingScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const LandingScreen = () => {
+  return (
+    <View>
+      <Text>This is Landing Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default LandingScreen;
